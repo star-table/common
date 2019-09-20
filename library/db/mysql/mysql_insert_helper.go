96cee9ce4e210db54442c6341d553c5d1471e9ed
Tree <tree@ejyi.com>
2019-09-20 16:48:43 +0800

    添加insert返回id方法

--- library/db/mysql/mysql_insert_helper.go
+++ library/db/mysql/mysql_insert_helper.go
@@ -22,12 +22,37 @@ func Insert(obj Domain) error {
 	return nil
 }
 
+func InsertReturnId(obj Domain) (interface{}, error) {
+	conn, err := GetConnect()
+	defer func() {
+		if err := conn.Close(); err != nil {
+			logger.GetDefaultLogger().Info(err)
+		}
+	}()
+	if err != nil {
+		return nil, err
+	}
+	id, err := conn.Collection(obj.TableName()).Insert(obj)
+	if err != nil {
+		return nil, err
+	}
+	return id, nil
+}
+
 func TransInsert(tx sqlbuilder.Tx, obj Domain) error {
 	_, err := tx.Collection(obj.TableName()).Insert(obj)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func TransInsertReturnId(tx sqlbuilder.Tx, obj Domain) (interface{}, error) {
+	id, err := tx.Collection(obj.TableName()).Insert(obj)
+	if err != nil {
+		return nil, err
+	}
+	return id, nil
 }
 
 func TransBatchInsert(tx sqlbuilder.Tx, obj Domain, objs []interface{}) error {
