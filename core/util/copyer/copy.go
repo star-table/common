61f68cd10904191041d8f3c181d62f2bd99b6036
Tree <tree@ejyi.com>
2019-10-24 17:53:57 +0800

    调整日志

--- core/util/copyer/copy.go
+++ core/util/copyer/copy.go
@@ -3,6 +3,7 @@ package copyer
 import (
 	"gitea.bjx.cloud/allstar/common/core/logger"
 	"gitea.bjx.cloud/allstar/common/core/util/json"
+	"gitea.bjx.cloud/allstar/common/core/util/strs"
 	"github.com/pkg/errors"
 )
 
@@ -11,12 +12,12 @@ var log = logger.GetDefaultLogger()
 func Copy(src interface{}, source interface{}) error {
 	jsonStr, err := json.ToJson(src)
 	if err != nil {
-		log.Error(err)
+		log.Error(strs.ObjectToString(err))
 		return errors.New("json转换异常")
 	}
 	err = json.FromJson(jsonStr, source)
 	if err != nil {
-		log.Error(err)
+		log.Error(strs.ObjectToString(err))
 	}
 	return nil
 }
